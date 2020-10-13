a928a5f3a79000e216e8a1756359fe0fcb5b776b
aishatsule <suleamaaisha@yahoo.com>
2020-10-13 12:43:25 +0200

    cleaned up code. added ui. added comments.

--- js-numerals/src/App.tsx
+++ js-numerals/src/App.tsx
@@ -1,29 +1,46 @@
 import React, {FC, useState} from 'react';
+import './NumberConverter.css';
 
 const App:FC = () => {
+
   const [input, setInput] = useState();
   const [output, setOutput] = useState();
 
+  //1-9
+  let belowTwenty = ["", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "Seventeen", "Eighteen", "Nineteen"];
+
+  //20-90
+  let tens = ["", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"];
+
+
+  //input is assumed to be a number
   const converter = (value:number) => {
 
     if (!isValid(value)) {
+      //Confirm if input is a valid number
       return false;
     }
 
+    //billion
     let billion = Math.floor(value / 1000000000);
     value -= billion * 1000000000;
 
+    //million
     let million = Math.floor(value / 1000000);
     value -= million * 1000000;
 
+    //thousand
     let thousand = Math.floor(value / 1000);
     value -= thousand * 1000;
 
+    //hundred
     let hundred = Math.floor(value / 100);
     value = value % 100;
 
+    //ten
     let ten = Math.floor(value / 10);
 
+    //one
     let one = Math.floor(value % 10);
 
     let words = "";
@@ -44,17 +61,15 @@ const App:FC = () => {
       words += (((words === "") ? "" : " ") + converter(hundred) + " hundred");
     }
 
-    let belowTwenty = ["", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "Seventeen", "Eighteen", "Nineteen"];
-    let tens = ["", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"];
-
     if (ten > 0 || one > 0) {
       if (words !== "") {
-        words += " and ";
+        words += " and ";// Add 'and'
+
       }
       if (ten < 2) {
         words += belowTwenty[ten * 10 + one];
       } else {
-        words += tens[ten];
+        words += tens[ten];// Add a hyphen if double digit
         if (one > 0) {
           words += ("-" + belowTwenty[one]);
         }
@@ -73,13 +88,21 @@ const App:FC = () => {
     return true
   }
 
+  const handleChange = (e:  React.ChangeEvent<HTMLInputElement>) => {
+    setInput(e.target.value)
+  }
+
+  const handleClick = () => {
+    converter(input)
+  }
+
   return (
     <>
       <div  className={"number-converter"}>Number Converter</div>
       <div  className={"number-box"}>
-      <input onChange={(e) => setInput(e.target.value)}/>
-        <button onClick={() => converter(input)}>Submit</button>
-        {output && <p>Output: {output}</p>}
+      <input onChange={handleChange}/>
+        <button className={"button"} onClick={handleClick}>Submit</button>
+        {output && <p> {output}</p>}
       </div>
     </>
   );
